cde1203c95584adbd90ae371109cab9b27947e20
Dimitris Bozelos <dbozelos@gmail.com>
2016-06-09 21:05:00 +0000

    Replace '$scope' with 'scope' in directive
    The '$' sign is commonly used for injected variables. 'scope' is
    therefore used in the 'link' function in directives because it is a
    normal function argument.

--- src/js/directives.js
+++ src/js/directives.js
@@ -15,11 +15,11 @@ angular.module('kcWidget')
       scope: {
         group: '='
       },
-      link: function($scope) {
+      link: function(scope) {
         // Watch the 'group' variable that holds the ids of the widgets, so that
         // we can automatically fetch and render the new widgets when the
         // 'group' to render has changed.
-        $scope.$watch('group', function(newValue, oldValue) {
+        scope.$watch('group', function(newValue, oldValue) {
 
           // There's nothing to do if the new group is the same with the old
           // group, as the widgets must have already been rendered. We can get
@@ -27,19 +27,19 @@ angular.module('kcWidget')
           // user clicks on a UI element, and the user clicks twice on the same
           // element.
           // We also get the same values the first time the 'group' is given,
-          // thus the check whether '$scope.widgets' is defined.
-          if ($scope.widgets && angular.equals(newValue, oldValue)) {
+          // thus the check whether 'scope.widgets' is defined.
+          if (scope.widgets && angular.equals(newValue, oldValue)) {
             return;
           }
 
           // Trigger the loading indicator.
-          $scope.dataReady = false;
+          scope.dataReady = false;
 
           kcWidgetApiFactory
             .group(newValue._id, newValue.filters, false)
             .then(function success(data) {
-              $scope.widgets   = data.widgets;
-              $scope.dataReady = true;
+              scope.widgets   = data.widgets;
+              scope.dataReady = true;
             })
           ;
 
